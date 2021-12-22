9ea4834a36822bb016987087b54536e053b4e02f
Bzelga <kogdanadomilonoegonet@mail.ru>
2021-12-23 00:55:34 +0500

    добавлен клиент, где есть страница для одной таблицы

---
+++ 7/SevenLab/SevenLab/DB/Games.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SevenLab
+{
+    public partial class Games
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int GamesId { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public int DeveloperId { get; set; }
+        [ForeignKey("DeveloperId")]
+        public Developers Developer { get; set; }
+
+        public int GenreId { get; set; }
+        [ForeignKey("GenreId")]
+        public Genre Genre { get; set; }
+    }
+}
---
+++ 7/SevenLab/SevenLab/DataAccess/Query.cs
@@ -0,0 +1,58 @@
+﻿using HotChocolate;
+using HotChocolate.Data;
+using HotChocolate.Subscriptions;
+using HotChocolate.Types;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SevenLab
+{
+    public class Query
+    {
+        [UsePaging]
+        [UseFiltering]
+        [UseSorting]
+        public List<Games> AllGameOnly([Service] GameRepository gameRepository) =>
+            gameRepository.GetGame();
+
+        [UsePaging]
+        [UseFiltering]
+        [UseSorting]
+        public List<Games> AllGameWithDevGerne([Service] GameRepository gameRepository) =>
+            gameRepository.GetGameWithGerneDeveloper();
+
+        [UseFiltering]
+        public async Task<Games> GetEmployeeById([Service] GameRepository gameRepository,
+            [Service] ITopicEventSender eventSender, int id)
+        {
+            Games gottenEmployee = gameRepository.GetGameById(id);
+            await eventSender.SendAsync("ReturnedGame", gottenEmployee);
+            return gottenEmployee;
+        }
+
+        [UsePaging]
+        [UseFiltering]
+        [UseSorting]
+        public List<Genre> AllGenreOnly([Service] GenreRepository genreRepository) =>
+            genreRepository.GetAllGenreOnly();
+
+        [UsePaging]
+        [UseFiltering]
+        [UseSorting]
+        public List<Genre> AllGenreWithGame([Service] GenreRepository genreRepository) =>
+            genreRepository.GetAllGenreWithGames();
+
+        [UsePaging]
+        [UseFiltering]
+        [UseSorting]
+        public List<Developers> AllDeveloperOnly([Service] DeveloperRepository developerRepository) =>
+            developerRepository.GetAllDeveloperOnly();
+
+        [UsePaging]
+        [UseFiltering]
+        [UseSorting]
+        public List<Developers> AllDeveloperWithGame([Service] DeveloperRepository developerRepository) =>
+            developerRepository.GetAllDeveloperWithGames();
+
+    }
+}
